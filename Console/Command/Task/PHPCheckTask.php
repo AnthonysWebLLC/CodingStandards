6e70d24b3638f6b90b667f8fa7bdb7434ca268ab
Anthony <Anthony@AnthonysWeb.com>
2013-03-27 21:55:35 -0700

    Strip errors generated from legacy variable names in .ctp files
    *Details*
    * History & threads indicate we shouldn't remove support in CakePHP 2.X
    ** https://github.com/cakephp/cakephp-codesniffer/pull/9
    ** https://github.com/cakephp/cakephp-codesniffer/issues/8
    * This method felt like less of a hack than modifying the CakePHP Sniffs
    ** Unsure if the CakePHP Sniffs were even meant to be used for .ctp files

--- Console/Command/Task/PHPCheckTask.php
+++ Console/Command/Task/PHPCheckTask.php
@@ -30,9 +30,11 @@ class PHPCheckTask extends StyleCheckTask {
 
         if ($summary) {
             exec("phpcs --warning-severity=0 --extensions=ctp,php --standard=CakePHP --report=summary $path", $result);
+			$result = $this->_strip_legacy_camel_case_errors($result);
             return empty($result) && $parentOutput;
         } else {
             exec("phpcs --warning-severity=0 --extensions=ctp,php --standard=CakePHP $path", $result);
+			$result = $this->_strip_legacy_camel_case_errors($result);
             $output = implode("\r\n", $result);
 			if(strlen($output)){
 				$return = "$parentOutput\r\nPHP Coding Standards errors:\r\n$output\r\n";
@@ -43,4 +45,37 @@ class PHPCheckTask extends StyleCheckTask {
         }
 
     }
+
+	private function _strip_legacy_camel_case_errors($result_lines){
+		$result_lines = $this->_strip_errors($result_lines, "Variable \"title_for_layout\" is not in valid camel caps format");
+		$result_lines = $this->_strip_errors($result_lines, "Variable \"scripts_for_layout\" is not in valid camel caps format");
+		$result_lines = $this->_strip_errors($result_lines, "Variable \"page_title\" is not in valid camel caps format");
+
+		return $result_lines;
+	}
+
+	private function _strip_errors($result_lines, $search){
+		// Strip individual error lines
+		$removed_count = 0;
+		foreach($result_lines AS $line_number=>$result_line){
+			if(false !== strpos($result_line, $search)){
+				unset($result_lines[$line_number]);
+				$removed_count++;
+			}
+		}
+
+		if($removed_count){
+			// Fix top error summary
+			preg_match('/^FOUND ([0-9]+) ERROR\\(S\\) AFFECTING ([0-9]+) LINE\\(S\\)$/', $result_lines[3], $matches);
+			$errors = $matches[1] - 1;
+			$lines = $matches[2] - 1;
+
+			// No errors so return empty output as it would have otherwise
+			if(0 === $errors){
+				return array();
+			}
+		}
+
+		return $result_lines;
+	}
 }
