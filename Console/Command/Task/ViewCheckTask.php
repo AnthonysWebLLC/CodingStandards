47b661df489339f9073558b6df46a5240fe66c2a
Anthony <Anthony@AnthonysWeb.com>
2013-06-04 23:34:07 +0000

    Hacking out view coding standard checks as they're not really ready for use

--- Console/Command/Task/ViewCheckTask.php
+++ Console/Command/Task/ViewCheckTask.php
@@ -3,7 +3,9 @@ App::uses('PHPCheckTask', 'CodingStandards.Console/Command/Task');
 
 class ViewCheckTask extends PHPCheckTask {
 
-	protected $_exts = array('ctp');
+	// Hacking out ctp inclusion as the coding standards there aren't really ready; It's just doing php checking which isn't good for views -- Anthony June 4th 2013
+	// protected $_exts = array('ctp');
+	protected $_exts = array('[ViewCodingStandardsChecksSuppressed]');
 
 	public function __construct() {
 		parent::__construct();
